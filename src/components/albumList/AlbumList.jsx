b87860fb221746458579caf2fd23aabee5dccb5f
Matthew-Hodgetts <mpolar999@gmail.com>
2021-05-12 19:13:14 +0100

    Added data from API to albums.json temporarily because of limit on API. Made albumList component to request the data then display albums wrapping them in a link to 'songs'/trackList component

--- src/components/albumList/AlbumList.jsx
+++ src/components/albumList/AlbumList.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
-const AlbumList = () => {
-  return <div className="albumList">Album list</div>;
+import { Link } from '@reach/router';
+import PropTypes from 'prop-types';
+
+const AlbumList = ({ albums }) => {
+  return (
+    <div className="albumList">
+      <ul>
+        {albums.map(({ album }) => (
+          <Link to={`/songs/${album.album_id}`} key={album.album_id}>
+            <li>{album.album_name}</li>
+          </Link>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default AlbumList;
+
+AlbumList.propTypes = {
+  albums: PropTypes.array,
+};
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import { Router } from '@reach/router';
+import axios from 'axios';
 // Import components
 import AlbumList from '../albumList/AlbumList';
 import Lyrics from '../lyrics/Lyrics';
 import TrackList from '../trackList/TrackList';
 
 const App = () => {
+  // State
+  const [data, setData] = useState({ albumList: [] });
+
+  // Call the musixmatch api for info about beatles albums
+  useEffect(() => {
+    console.log('mounted');
+    axios
+      .get('/albums.json')
+      .then(function (response) {
+        // handle success
+        console.log(response);
+        setData({ ...data, albumList: response.data.message.body.album_list });
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  }, []);
+
   return (
     <div className="App">
-      <AlbumList />
-      <Lyrics />
-      <TrackList />
+      <Router>
+        <AlbumList path="/" albums={data.albumList} />
+        <Lyrics path="lyrics" />
+        <TrackList path="songs" />
+      </Router>
     </div>
   );
 };
